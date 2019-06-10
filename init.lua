1f34df2eb15732075fa0187e08699c57d567f0b0
bosapara <38753015+bosapara@users.noreply.github.com>
2019-06-10 17:16:07 +0400

    update
    update

--- init.lua
+++ init.lua
@@ -1,64 +1,29 @@
-
 --Freedom from Android users
 
 minetest.register_on_joinplayer(function(player)
 	
-	
 	if minetest.check_player_privs(player:get_player_name(), {ban=true})==false then
-	
 	
 		local name = player:get_player_name()
 		local privs = minetest.get_player_privs(name)
 		privs.interact = nil
+		privs.shout = nil
 		minetest.set_player_privs(name, privs)
 	
 		local num = math.random(1000,9999)
 		player:set_attribute("ffa", num)
 		local num2 = player:get_attribute("ffa")
 		
-		minetest.show_formspec(player:get_player_name(), "ffa_form", "size[8,7] bgcolor[#333444cc; false] image[2.9,1;3.5,3.5;login_icon.png] button_exit[2.9,5.4;3,0.5;OK1;JOIN] pwdfield[3.2,4.55;3,1;pass;;#cccccc] tooltip[OK1;Your password: "..num2..";#A500A5;white] vertlabel[0.2,0.5;CHECK BUTTON TOOLTIP] box[-0.3,-0.3;1.1,7.7;#111111]")
+		minetest.show_formspec(player:get_player_name(), "ffa_form", "size[6.5,5] background[1,1;1,1;gui_formbg.png;true] bgcolor[#111111aa; true] image_button[0.7,1.3;2,2;key_logo.png;OK2;] button_exit[3.45,4.25;2.9,0.5;OK1;enter] pwdfield[0.7,4.35;2.8,1;pass;;#cccccc] tooltip[OK2;Your password: "..num2..";#A500A5;white] label[3,1.9;Hover key\nto reveal password] box[0,0;6.35,0.7;#111111] label[0.3,0.15;"..minetest.colorize("#777777","Server available for PC users only").."]")
 		
+
 		
-	
 	end
 	
 end)
 
-minetest.register_chatcommand("join", {
-  param = "<test>",
-  description = "Password for interact",
-  privs = {shout=true},
-  func = function(name, param)
-
-	if param ~= "" then
-	return
-	end
-
-	local player = minetest.get_player_by_name(name)
-
-	if minetest.check_player_privs(player:get_player_name(), {interact=true})==false then
-
-		local num2 = player:get_attribute("ffa")
-		
-		minetest.show_formspec(player:get_player_name(), "ffa_form", "size[8,7] bgcolor[#333444cc; false] image[2.9,1;3.5,3.5;login_icon.png] button_exit[2.9,5.4;3,0.5;OK1;JOIN] pwdfield[3.2,4.55;3,1;pass;;#cccccc] tooltip[OK1;Your password: "..num2..";#A500A5;white] vertlabel[0.2,0.5;CHECK BUTTON TOOLTIP] box[-0.3,-0.3;1.1,7.7;#111111]")
-
-	else
-	
-	minetest.chat_send_player(name, (minetest.colorize("#61FF00","You have interact, "..name.."!")))
-	
-	
-	end
-
-
-
-  end,
-})
-
-
-
 minetest.register_on_player_receive_fields(function(player, formname, fields)
-	if formname == "ffa_form" then -- Replace this with your form name
-		--fields.name = num
+	if formname == "ffa_form" then
 		if fields.OK1 then
 			local number = player:get_attribute("ffa")
 			if fields.pass == number then
@@ -66,19 +31,18 @@ minetest.register_on_player_receive_fields(function(player, formname, fields)
 				local name = player:get_player_name()
 				local privs = minetest.get_player_privs(name)
 				privs.interact = true
+				privs.shout = true
 				minetest.set_player_privs(name, privs)
-
-
 
 				minetest.chat_send_player(name, (minetest.colorize("#61FF00","Congrats, now you can play, "..name.."!")))
 				
-				
-			else
-				--minetest.kick_player(name, "Your password is wrong, try to join again!")
-				minetest.chat_send_player(name, (minetest.colorize("red","Your password is wrong, try again with command /join, "..name.."!")))
+			elseif fields.pass ~= number then
+				minetest.kick_player(name, "Your password is wrong, try to join again!")
 			end
+		
+		elseif fields.quit == "true" then
+				minetest.kick_player(name, "Password is wrong try to join again!")	
 		end
 		
 	end
 end)
-
