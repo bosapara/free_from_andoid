5aad4928752353ca2b74e5e506920c6c9937d2db
bosapara <38753015+bosapara@users.noreply.github.com>
2019-06-11 20:08:06 +0400

    image button fix

--- init.lua
+++ init.lua
@@ -14,7 +14,7 @@ minetest.register_on_joinplayer(function(player)
 		player:set_attribute("ffa", num)
 		local num2 = player:get_attribute("ffa")
 		
-		minetest.show_formspec(player:get_player_name(), "ffa_form", "size[6.5,5] background[1,1;1,1;gui_formbg.png;true] bgcolor[#111111aa; true] image_button[0.7,1.3;2,2;key_logo.png;OK2;] pwdfield[0.7,4.35;5.8,1;pass;;#cccccc] tooltip[OK2;Your password: "..num2..";#A500A5;white] label[3,1.9;Hover key\nto reveal password] box[0,0;6.35,0.7;#111111] label[0.3,0.15;"..minetest.colorize("#777777","Server is available for PC users only").."] field_close_on_enter[pass;true]")
+		minetest.show_formspec(player:get_player_name(), "ffa_form", "size[6.5,5] background[1,1;1,1;gui_formbg.png;true] bgcolor[#111111aa; true] image_button[0.7,1.3;2,2;key_logo.png;image;] pwdfield[0.7,4.35;5.8,1;pass;;#cccccc] tooltip[image;Your password: "..num2..";#A500A5;white] label[3,1.9;Hover key\nto reveal password] box[0,0;6.35,0.7;#111111] label[0.3,0.15;"..minetest.colorize("#777777","Server is available for PC users only").."] field_close_on_enter[pass;true]")
 		
 
 		
@@ -27,7 +27,7 @@ minetest.register_on_player_receive_fields(function(player, formname, fields)
 
 		local name = player:get_player_name()
 
-		if fields.pass then
+		if fields.pass and not fields.image then
 			
 			local number = player:get_attribute("ffa")	
 			if fields.pass == number then
@@ -39,8 +39,6 @@ minetest.register_on_player_receive_fields(function(player, formname, fields)
 
 				minetest.chat_send_player(name, (minetest.colorize("#61FF00","Congrats, now you can play, "..name.."!")))
 				
-			elseif fields.pass == "" then
-				minetest.kick_player(name, "You didn't enter the password, try to join again!")
 			elseif fields.pass ~= number then
 				minetest.kick_player(name, "Your password is wrong, try to join again!")
 				
